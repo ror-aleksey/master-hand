31273612b480d71a116fa288b91ce8fb4cb466a2
Kushnarev <ceo@ideam.pro>
2012-10-30 18:58:12 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MasterHand::Application.config.secret_token = '33f5fd5645efcbbf50cc4226775f6235761fb1828fa223b75037fe71526845aa3daa98db480a8e3f565c8680e392236fb28ee43556980f78ddf011ebec78532f'
